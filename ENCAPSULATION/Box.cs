3092eb78fc5f0fdd9483250537a2d6bd90bf9564
boretobotev <boris.botev97@gmail.com>
2018-10-25 10:03:11 +0300

    Class box done

---
+++ ENCAPSULATION/Box.cs
@@ -0,0 +1,45 @@
+﻿namespace ENCAPSULATION
+{
+    using System;
+    using System.Text;
+
+    public class Box
+    {
+        
+        public Box(double lenght,double width, double height)
+        {
+            this.Lenght = lenght;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Lenght { get; set; }
+
+        private double CalcArea()
+        {
+            return (2 * this.Lenght * this.Width) + (2 * this.Lenght * this.Height) + (2 * this.Height * this.Width);
+        }
+
+        private double CalcLateralArea()
+        {
+            return (2 * this.Lenght * this.Height) + (2 * this.Height * this.Width);
+        }
+
+        private double CalcVolume()
+        {
+            return this.Height * this.Width * this.Lenght;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder(); 
+            sb.AppendLine($"Surface Area – {CalcArea():f2}");
+            sb.AppendLine($"Lateral Surface Area – {CalcLateralArea():f2}");
+            sb.AppendLine($"Volume – {CalcVolume():f2}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ENCAPSULATION/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace ENCAPSULATION
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var lenght = double.Parse(Console.ReadLine());
+            var widht = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            Box box = new Box(lenght, widht, height);
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
